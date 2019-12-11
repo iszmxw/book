ab429a863915c58fa99c49cb4175382a2d06eead
追梦小窝 <mail@54zm.com>
2019-12-11 19:59:15 +0800

    先下拉=>后上传

--- Application/Home/Controller/BookController.class.php
+++ Application/Home/Controller/BookController.class.php
@@ -37,6 +37,7 @@ class BookController extends HomeController
 //        $dd->setParam($this->_mp);
         $jssdk = $dd->getsignpackage();
         $this->assign('jssdk', $jssdk);
+        dump($_site);
         $this->display();
     }
 
