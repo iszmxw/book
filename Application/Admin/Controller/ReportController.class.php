2435878e7618a6f8b90f2c95800e8a406650c6a2
iszmxw <mail@54zm.com>
2019-12-14 16:10:02 +0800

    先下拉=>后上传

--- Application/Admin/Controller/ReportController.class.php
+++ Application/Admin/Controller/ReportController.class.php
@@ -34,6 +34,7 @@ class ReportController extends AdminController
         }
         $this->assign('data', json_encode($data));
         $this->assign('cates', json_encode($cates));
+        dd($view_data);
         $this->views($view_data);
         $this->display();
     }
