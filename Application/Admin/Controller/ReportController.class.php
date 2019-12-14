635fb6b393155f8431e5ffcda41c9bf313659dc5
iszmxw <mail@54zm.com>
2019-12-14 16:16:20 +0800

    先下拉=>后上传

--- Application/Admin/Controller/ReportController.class.php
+++ Application/Admin/Controller/ReportController.class.php
@@ -15,12 +15,14 @@ class ReportController extends AdminController
             'stime' => $stime,
             'etime' => $etime,
         ];
+        $limit     = 0;
         if ($stime && $etime) {
             $stime         = strtotime(I('stime'));
             $etime         = strtotime(I('etime'));
             $where['date'] = array(array('gt', date('Ymd', $stime)), array('lt', date('Ymd', $etime)));
+        } else {
+            $limit = 7;
         }
-        $limit = 7;
         // 查询最近七天的报表
         $list = M('data')->limit($limit)->where($where)->order('date desc')->select();
         dump($list);
