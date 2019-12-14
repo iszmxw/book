3d1d91a62fe516bd27fa069e031afd7406ee9ce0
iszmxw <mail@54zm.com>
2019-12-14 16:34:34 +0800

    先下拉=>后上传

--- Application/Admin/Controller/TreeController.class.php
+++ Application/Admin/Controller/TreeController.class.php
@@ -1,16 +1,28 @@
 <?php
 
 namespace Admin\Controller;
-
+/**
+ * 用户树形关系控制器
+ * Class TreeController
+ * @package Admin\Controller
+ */
 class TreeController extends AdminController
 {
-    // 通知列表
+    /**
+     * 用户树形关系
+     * @author: iszmxw <mail@54zm.com>
+     * @Date：2019/12/14 16:34
+     */
     public function index()
     {
         $this->display();
     }
 
-    // 获取数据
+    /**
+     * 获取数据
+     * @author: iszmxw <mail@54zm.com>
+     * @Date：2019/12/14 16:33
+     */
     public function get_users()
     {
         $id   = intval(I('id'));
