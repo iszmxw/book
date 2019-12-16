f0516b8ec8df736857ac6a4827f890a9a9678b74
追梦小窝 <mail@54zm.com>
2019-12-17 00:08:45 +0800

    先下拉=>后上传

--- Application/Collection/Controller/IndexController.class.php
+++ Application/Collection/Controller/IndexController.class.php
@@ -52,7 +52,7 @@ class IndexController extends CollectionController
 //        }
         $url     = "https://www.biquge.com.cn/book/32883/196851.html";
         $ql      = QueryList::get($url);
-        $content = $ql->find('#content')->text(); // 获取小说内容
+        $content = $ql->find('#content')->html(); // 获取小说内容
         echo($content);
     }
 
