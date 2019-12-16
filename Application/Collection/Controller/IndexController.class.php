e3d3c61510e1b342f59bb86e4b8c2a2502f90025
追梦小窝 <mail@54zm.com>
2019-12-17 00:56:32 +0800

    先下拉=>后上传

--- Application/Collection/Controller/IndexController.class.php
+++ Application/Collection/Controller/IndexController.class.php
@@ -46,16 +46,17 @@ class IndexController extends CollectionController
         $redis->connect('118.89.61.124', 4399);
         $redis->auth('blog_54zm_com');              //密码验证
         $redis->select(2);                          //选择数据库2
-        while (1) {
-            $res = $redis->lpop('iszmxw');
+        $res = $redis->lpop('iszmxw');
 //            if ($res) {
 //                echo $res;
 //                $data = json_decode($res);
 //                $this->handlezip($data['title'], $data['url']);
 //            }
-            if ($res) {
-                echo $res . "\r\n";
-            }
+        if ($res) {
+            echo $res . "\r\n";
+        }
+        while (1) {
+
             sleep(1);
         }
 
