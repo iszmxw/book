c8bcd4348e095177b4ddaa139afa401d6d884ce4
追梦小窝 <mail@54zm.com>
2019-12-16 20:20:42 +0800

    先下拉=>后上传

--- Application/Collection/Controller/IndexController.class.php
+++ Application/Collection/Controller/IndexController.class.php
@@ -12,6 +12,8 @@ class IndexController extends CollectionController
         $url   = "https://www.biquge.com.cn/book/32883/";
         $ql    = QueryList::get($url);
         $title = $ql->find('title')->text(); // 获取网站标题
-        dump($title);
+        $texts = $ql->find('dd>a')->texts(); //获取搜索结果标题列表
+        $hrefs = $ql->find('dd>a')->attrs('href'); //获取搜索结果链接列表
+        dump($title,$texts,$hrefs);
     }
 }
