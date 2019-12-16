640c305eb52f4e8f048d3740024e6b92d34b83dc
追梦小窝 <mail@54zm.com>
2019-12-16 20:21:03 +0800

    先下拉=>后上传

--- Application/Collection/Controller/IndexController.class.php
+++ Application/Collection/Controller/IndexController.class.php
@@ -7,6 +7,11 @@ use QL\QueryList;
 
 class IndexController extends CollectionController
 {
+    /**
+     *
+     * @author：iszmxw <mail@54zm.com>
+     * @time：2019/12/16 20:20
+     */
     public function index()
     {
         $url   = "https://www.biquge.com.cn/book/32883/";
@@ -14,6 +19,6 @@ class IndexController extends CollectionController
         $title = $ql->find('title')->text(); // 获取网站标题
         $texts = $ql->find('dd>a')->texts(); //获取搜索结果标题列表
         $hrefs = $ql->find('dd>a')->attrs('href'); //获取搜索结果链接列表
-        dump($title,$texts,$hrefs);
+        dump($title, $texts, $hrefs);
     }
 }
